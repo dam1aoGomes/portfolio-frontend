3e1abfbabec743801c797e2786361ecd3808970a
Damião Gomes <damiao28.contato@gmail.com>
2025-02-11 12:39:15 -0300

    add index page, hero and cards

---
+++ components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hero bg-base-200 h-1/2">
+        <div class="hero-content flex-col lg:flex-row">
+            <img src="/images/damiao.jpg" class="w-1/5 rounded-full shadow-2xl" />
+            <div>
+                <h1 class="text-3xl font-bold">Damião Gomes</h1>
+                <p class="py-6">
+                    Desenvolvedor Full Stack com experiência em TypeScript, Node.js, Flask e FastAPI.
+                    Apaixonado por criar soluções eficientes e escaláveis, utilizando Tailwind para um design moderno e
+                    responsivo.
+                </p>
+                <div class="flex w-1/6 justify-between">
+                    <a>
+                        <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" class="w-1/2">
+                    </a>
+                    <a>
+                        <img src="https://img.icons8.com/?size=100&id=ARy6tFUfwclb&format=png&color=000000" class="w-1/2">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <Hero />
+    <h2 class="text-3xl font-bold p-10">Conheça os meus projetos</h2>
+    <Cards />
+</template>
