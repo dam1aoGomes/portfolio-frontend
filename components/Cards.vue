2c32da35ace33a571f3c68e210d40dff43d71be8
Damião Gomes <damiao28.contato@gmail.com>
2025-02-11 19:33:49 -0300

    add cards

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,42 +1,22 @@
 <template>
-  <div class="flex wrap justify-around">
-    <div class="card card-compact bg-base-100 w-96 shadow-xl">
+  <div class="flex items-start">
+    <div class="card bg-base-100 image-full w-96 shadow-xl m-10">
       <figure>
-        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+        <img src="/public/images/postlab-card2.png" alt="Shoes" />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 class="card-title">PostLab</h2>
+        <p>O PostLab é uma plataforma dinâmica de postagens que permite compartilhar ideias, artigos e projetos de forma intuitiva e organizada.</p>
+        <div class="card-actions justify-start">
+          <img src="/public/favicon.ico" class="size-7">
+          <img src="https://icon.icepanel.io/Technology/svg/Tailwind-CSS.svg" class="size-7">
+          <img src="https://icon.icepanel.io/Technology/svg/Python.svg" class="size-7">
+        </div>
         <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+          <button class="btn btn-primary">Conheça</button>
         </div>
       </div>
     </div>
     <!---->
-    <div class="card card-compact bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-    <!---->
-    <div class="card card-compact bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hero bg-base-200 h-1/2">
         <div class="hero-content flex-col lg:flex-row">
-            <img src="/images/damiao.jpg" class="w-1/5 rounded-full shadow-2xl" />
+            <img src="/images/damiao.jpg" class="w-1/2 md:w-1/5 rounded-full shadow-2xl" />
             <div>
                 <h1 class="text-3xl font-bold">Damião Gomes</h1>
                 <p class="py-6">
@@ -9,7 +9,7 @@
                     Apaixonado por criar soluções eficientes e escaláveis, utilizando Tailwind para um design moderno e
                     responsivo.
                 </p>
-                <div class="flex w-1/6 justify-between">
+                <div class="flex w-1/3 md:w-1/6 md:justify-between">
                     <a>
                         <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" class="w-1/2">
                     </a>
