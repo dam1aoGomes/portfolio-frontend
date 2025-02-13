bb075a143c84025fe7cb1a1b8bdeb54570f4b476
Damiao Gomes <damiao28.contato@gmail.com>
2025-02-13 19:41:32 -0300

    update postlab page

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,13 +3,16 @@
         <div class="hero-content flex-col lg:flex-row">
             <img src="/images/damiao.jpg" class="w-1/2 md:w-1/5 rounded-full shadow-2xl" />
             <div>
-                <h1 class="text-3xl font-bold">Damião Gomes</h1>
-                <p class="py-6">
+                <h1 class="text-3xl font-bold md:ml-20">Damião Gomes</h1>
+                <p class="text-md max-w-2xl mt-2 text-justify md:ml-20">
                     Desenvolvedor Full Stack com experiência em TypeScript, Node.js, Flask e FastAPI.
                     Apaixonado por criar soluções eficientes e escaláveis, utilizando Tailwind para um design moderno e
                     responsivo.
                 </p>
-                <div class="flex w-1/3 md:w-1/6 md:justify-between">
+                <p class="text-md max-w-2xl mt-2 text-justify md:ml-20">
+                    Estudante de Ciência da Computação na Universidade Federal do Ceará,
+                    sempre em busca de novos desafios e aprendizado contínuo.</p>
+                <div class="flex w-1/3 md:w-1/6 md:justify-between mt-10 md:ml-20">
                     <a>
                         <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" class="w-1/2">
                     </a>
--- pages/project/postlab.vue
+++ pages/project/postlab.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--Hero-->
   <div class="flex flex-col items-center">
     <div class="hero bg-base-200 min-h-screen">
       <div class="hero-content text-center">
@@ -11,42 +12,31 @@
             Desenvolvido com tecnologia moderna, o site oferece uma interface minimalista e responsiva,
             garantindo uma experiência fluida tanto para escritores quanto para leitores.
           </p>
-          <button class="btn btn-primary">Conheça</button>
+          <button class="btn btn-neutral m-1">Ideia</button>
+          <button class="btn btn-primary m-1">Tecnologias</button>
+          <button class="btn btn-secondary m-1">Design</button>
         </div>
       </div>
     </div>
-    <!---->
-    <div class="mockup-browser bg-base-300 border m-5" v-if="showWebBrowse">
-      <div class="mockup-browser-toolbar">
-        <div class="input">https://postlab-frontend.vercel.app/</div>
-      </div>
-      <div class="bg-base-200 flex justify-center ">
-        <img src="/public/images/postlab-web-browser.png" class="">
-      </div>
-    </div>
-    <!---->
-    <div class="mockup-phone mt-20" v-if="showWebPhone">
-      <div class="camera"></div>
-      <div class="display">
-        <img src="/public/images/postlab-web-phone.png" class="mt-10">
+    <!--Ideia-->
+    <div class="hero bg-base-200 min-h-screen">
+      <div class="hero-content flex-col lg:flex-row">
+        <div>
+          <button class="btn btn-neutral m-1 text-5xl font-bold ml-10">Ideia</button>
+          <p class="py-6 text-justify m-10">
+            Desde que comecei meus estudos em 2019, sempre tive o desejo de criar um site de postagens simples, mas funcional. Com as ferramentas que eu conhecia na época — PHP, MySQL, HTML, CSS e JavaScript — desenvolvi diversos sites, porém todos eram bastante amadores.  
+            Naquele tempo, eu ainda não dominava autenticação de usuários nem paginação de posts, o que limitava bastante os projetos.  
+            Agora, decidi revisitar essa ideia e criar este projeto como uma homenagem ao meu eu do passado, mostrando o quanto evoluí ao longo dos anos, mas sem perder a essência inicial. 
+            <br> O <b>PostLab</b> é mais do que um simples site de postagens: é o primeiro marco do meu portfólio e uma prova do meu crescimento como desenvolvedor.
+          </p>
+        </div>
       </div>
     </div>
+    <!--Tecnologias-->
+    <div class="divider"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const showWebBrowse = ref(false);
-const showWebPhone  = ref(false);
 
-function changeExample() {
-  if(window.innerWidth >= 750) {
-    showWebBrowse.value = true;
-    showWebPhone.value = false;
-  } else {
-    showWebBrowse.value = false;
-    showWebPhone.value = true;  
-  }
-}
-
-onMounted(changeExample);
 </script>
