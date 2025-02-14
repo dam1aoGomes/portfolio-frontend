7cd7c1dfa638ced03f82ad2620d90b6cf23de163
Damiao Gomes <damiao28.contato@gmail.com>
2025-02-13 23:08:55 -0300

    add cards tecnologies, postlab page

---
+++ components/postlab/Cards.vue
@@ -0,0 +1,65 @@
+<template>
+<!--NUXT-->
+<div class="card bg-base-100 w-96 shadow-xl m-5">
+    <figure class="px-10 pt-10">
+        <img
+        src="https://icon.icepanel.io/Technology/svg/Nuxt-JS.svg"
+        alt="Shoes"
+        class="rounded-xl size-10" />
+    </figure>
+    <div class="card-body items-center text-center">
+        <h2 class="card-title">Nuxt</h2>
+        <p class="text-justify">Nuxt.js é um framework baseado em Vue.js que utilizei com SSR para otimizar desempenho e SEO. Ele facilita o desenvolvimento com roteamento automático e renderização eficiente.</p>
+        <div class="card-actions">
+        </div>
+    </div>
+</div>
+
+<!--Tailwind-->
+<div class="card bg-base-100 w-96 shadow-xl m-5">
+    <figure class="px-10 pt-10">
+        <img
+        src="https://icon.icepanel.io/Technology/svg/Tailwind-CSS.svg"
+        alt="Shoes"
+        class="rounded-xl size-10" />
+    </figure>
+    <div class="card-body items-center text-center">
+        <h2 class="card-title">Tailwind</h2>
+        <p class="text-justify">Tailwind CSS + daisyUI foram utilizados para criar uma UI moderna, responsiva e estilizada rapidamente. O daisyUI adiciona componentes prontos, mantendo a flexibilidade do Tailwind. </p>
+        <div class="card-actions">
+        </div>
+    </div>
+</div>
+
+<!--FastAPI-->
+<div class="card bg-base-100 w-96 shadow-xl m-5">
+    <figure class="px-10 pt-10">
+        <img
+        src="https://icon.icepanel.io/Technology/svg/FastAPI.svg"
+        alt="Shoes"
+        class="rounded-xl size-10" />
+    </figure>
+    <div class="card-body items-center text-center">
+        <h2 class="card-title">FastAPI</h2>
+        <p class="text-justify">FastAPI + SQLModel foram utilizados para criar uma API rápida e eficiente, com tipagem robusta e suporte nativo a validação de dados. Essa combinação facilita o desenvolvimento e a integração com o banco de dados.</p>
+        <div class="card-actions">
+        </div>
+    </div>
+</div>
+
+<!--PostgresSQL-->
+<div class="card bg-base-100 w-96 shadow-xl m-5">
+    <figure class="px-10 pt-10">
+        <img
+        src="https://icon.icepanel.io/Technology/svg/PostgresSQL.svg"
+        alt="Shoes"
+        class="rounded-xl size-10" />
+    </figure>
+    <div class="card-body items-center text-center">
+        <h2 class="card-title">PostgresSQL</h2>
+        <p class="text-justify">PostgreSQL + Neon foram utilizados para um banco de dados escalável e eficiente na nuvem. O Neon oferece deploy simplificado, auto scaling e suporte a branches para desenvolvimento ágil. </p>
+        <div class="card-actions">
+        </div>
+    </div>
+</div>
+</template>
--- pages/project/postlab.vue
+++ pages/project/postlab.vue
@@ -20,20 +20,28 @@
     </div>
     <!--Ideia-->
     <div class="hero bg-base-200 min-h-screen">
-      <div class="hero-content flex-col lg:flex-row">
-        <div>
-          <button class="btn btn-neutral m-1 text-5xl font-bold ml-10">Ideia</button>
-          <p class="py-6 text-justify m-10">
-            Desde que comecei meus estudos em 2019, sempre tive o desejo de criar um site de postagens simples, mas funcional. Com as ferramentas que eu conhecia na época — PHP, MySQL, HTML, CSS e JavaScript — desenvolvi diversos sites, porém todos eram bastante amadores.  
-            Naquele tempo, eu ainda não dominava autenticação de usuários nem paginação de posts, o que limitava bastante os projetos.  
-            Agora, decidi revisitar essa ideia e criar este projeto como uma homenagem ao meu eu do passado, mostrando o quanto evoluí ao longo dos anos, mas sem perder a essência inicial. 
-            <br> O <b>PostLab</b> é mais do que um simples site de postagens: é o primeiro marco do meu portfólio e uma prova do meu crescimento como desenvolvedor.
-          </p>
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Ideia</h1>
+              <p class="py-6 text-justify">
+                Desde que comecei meus estudos em 2019, sempre tive o desejo de criar um site de postagens simples, mas funcional. Com as ferramentas que eu conhecia na época — PHP, MySQL, HTML, CSS e JavaScript — desenvolvi diversos sites, porém todos eram bastante amadores.
+                Naquele tempo, eu ainda não dominava autenticação de usuários nem paginação de posts, o que limitava bastante os projetos.
+                Agora, decidi revisitar essa ideia e criar este projeto como uma homenagem ao meu eu do passado, mostrando o quanto evoluí ao longo dos anos, mas sem perder a essência inicial.
+              </p>
+              <p class="py-6 text-justify">
+                O <b>PostLab</b> é mais do que um simples site de postagens: é o primeiro marco do meu portfólio e uma prova do meu crescimento como desenvolvedor.
+              </p>
+            <button class="btn btn-primary">Tecnologias</button>
+          </div>
         </div>
-      </div>
     </div>
     <!--Tecnologias-->
-    <div class="divider"></div>
+    <div class="w-full h-screen flex flex-col items-center">
+      <h1 class="text-5xl font-bold mt-10">Tecnologias</h1>
+      <div class="w-full flex flex-wrap items-center justify-center md:items-start md:justify-start">
+        <PostlabCards />
+      </div>
+    </div>
   </div>
 </template>
 
